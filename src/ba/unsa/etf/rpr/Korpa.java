194c41f02f70f5b89925aeb16b4230e2b146bc01
ehondo1 <ehondo1@etf.unsa.ba>
2019-10-19 19:23:54 +0200

    uradena klasa Korpa

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private final int maxBrArtikala=50;
+    private int kupljeno=0;
+    private Artikl[] kupljeniProizvodi=new Artikl[maxBrArtikala];
+
+    public boolean dodajArtikl(Artikl proizvod) {
+        if(kupljeno<maxBrArtikala) {
+            kupljeniProizvodi[kupljeno]=proizvod;
+            kupljeno++;
+            return true;
+        }
+        return false;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+            Artikl izbaceniArtikl=null;
+            for(int i=0;i<kupljeno;i++) {
+                izbaceniArtikl=kupljeniProizvodi[i];
+                if(kupljeniProizvodi[i].getKod().equals(kod)) {
+                    for(int j=i;j<kupljeno-1;j++)
+                        kupljeniProizvodi[j]=kupljeniProizvodi[j+1];
+                    kupljeno--;
+                    i--;
+                }
+            }
+            return izbaceniArtikl;
+    }
+    public int dajUkupnuCijenuArtikala() {
+        int suma=0;
+        for(int i=0;i<kupljeno;i++)
+            suma=suma+kupljeniProizvodi[i].getCijena();
+        return suma;
+    }
+    public Artikl[] getArtikli() {
+        return kupljeniProizvodi;
+    }
+
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private final int maxBrArtikala=1000;
+    private Artikl[] artikli=new Artikl[maxBrArtikala];
+    private int brArikala=0;
+
+    public vodi dodajArtikl(Artikl artikal) {
+        if(brArikala<maxBrArtikala)
+        {
+            artikli[brArikala]=artikal;
+            brArikala++;
+        }
+    }
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl proizvod=null;
+        for(int i=0;i<brArikala;i++) {
+            proizvod=artikli[i];
+            if(artikli[i].getKod().equals(kod)) {
+                for(int j=i;j<brArikala-1;j++)
+                    artikli[j]=artikli[j+1];
+                i--;
+                brArikala--;
+            }
+        }
+        return proizvod;
+    }
 }
